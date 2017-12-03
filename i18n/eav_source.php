a7670202e8f06a8ea6fc6587e93a6e16221f7f18
YeeSoft <makitrataras@gmail.com>
2017-12-03 13:45:51 -0500

    source message update

--- i18n/eav_source.php
+++ i18n/eav_source.php
@@ -1,9 +1,23 @@
 <?php
 
 return [
-    'yee/page' => [
-        'Page',
-        'Pages',
-        'Create Page',
+    'yee/eav' => [
+        'An error occurred during creation of EavValue record.',
+        'An error occurred during saving EAV attributes!',
+        'Attribute Options',
+        'Attribute Types',
+        'Attribute',
+        'Attributes',
+        'Available Attributes',
+        'Custom Fields',
+        'EAV',
+        'Entity Models',
+        'Entity was not found.',
+        'Entity',
+        'Model was not found.',
+        'Model',
+        'Option',
+        'Raw',
+        'Selected Attributes',
     ]
 ];
